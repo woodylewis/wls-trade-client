502166db84562b6f63a1bc716ced9a4cc63622aa
woodylewis <woody@woodylewis.com>
2015-02-13 12:40:51 -0500

    Change server URL, edit README

--- README.md
+++ README.md
@@ -6,6 +6,9 @@ Basic CRUD operations from AngularJS client to Node-Express-MongoDB server
 bower install angular-bootstrap
 ```
 ```
+bower install angular-ui-router
+```
+```
 bower install socket.io-client 
 ```
 ```
--- app/services/services.js
+++ app/services/services.js
@@ -1,7 +1,8 @@
 angular.module('services', [])
 
 .factory('socketService', function (socketFactory) {
-	var theIoSocket = io.connect('http://dev1:3500');
+	var theIoSocket = io.connect('http://mean.wlsllc.com:3500');
+  //var theIoSocket = io.connect('http://dev1:3500');
 
 	var theSocket = socketFactory({
 		ioSocket: theIoSocket
